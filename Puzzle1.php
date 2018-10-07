b764f9db118358830e9164163c9221c2f1a6b353
XQ22 <ryan@openi.biz>
2018-10-07 20:48:23 +0300

    Create Puzzle1.php

---
+++ Puzzle1.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<style>
+body {
+  background-color: #252636;
+  color: #acaeb2;
+  text-align:center;
+}
+  
+h1, h2 {
+  font-family: Arial, Helvetica Neue, monospace;
+}
+
+h1 {
+  font-size: 70px;
+}
+
+p {
+  font-family: Helvetica, Arial, monospace;
+}
+  
+table {
+  border: 0px;
+}
+
+button {
+  border: none;
+  -webkit-transition-duration: 0.5s;
+  transition-duration: 0.5s;
+}
+
+button:hover {
+  color: #ccced2;
+}
+
+img {
+	margin: 0px;
+	border: 0px;
+	padding: 0px;
+	width: 50px;
+}
+
+</style>
+</head>
+<body>
+<h2>Welcome, Rocket Penguin</h2>
+<?php
+
+?>
+</body>
+</html>
